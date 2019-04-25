2976cba3a55738176347da577a938796e8bc536c
Marcus Rose <marcusr2626@gmail.com>
2019-04-25 11:33:57 -0400

    Update RoseBot.java

--- RoseBot.java
+++ RoseBot.java
@@ -5,12 +5,12 @@ import robocode.*;
 // API help : http://robocode.sourceforge.net/docs/robocode/robocode/Robot.html
 
 /**
- * RoseBot - a robot by Marcus
+ * ClassBot - a robot by (your name here)
  */
 public class RoseBot extends AdvancedRobot
 {
 	/**
-	 * run: RoseBot's default behavior
+	 * run: ClassBot's default behavior
 	 */
 	public void run() {
 		// Initialization of the robot should be put here
@@ -21,21 +21,36 @@ public class RoseBot extends AdvancedRobot
 		// setColors(Color.red,Color.blue,Color.green); // body,gun,radar
 
 		// Robot main loop
+		setAdjustGunForRobotTurn(true);
+		setAdjustRadarForGunTurn(true);
+		setAdjustRadarForRobotTurn(true);
 
 		addCustomEvent(new RadarTurnCompleteCondition(this));
-
+		addCustomEvent(new MoveCompleteCondition(this));
 
 		while(true) {
 			// Replace the next 4 lines with any behavior you would like
 			execute();
-
 		}
 	}
 
-	public void onCustomEvent(CustomEvent e) {
-		if (e.getCondition() instanceof RadarTurnCompleteCondition) {
+	public void onCustomEvent(CustomEvent event) {
+		if (event.getCondition() instanceof RadarTurnCompleteCondition) {
 			setTurnRadarRight(360);
-
+		} else if (event.getCondition() instanceof MoveCompleteCondition) {
+			double newX = Math.random() * getBattleFieldWidth();
+			double newY = Math.random() * getBattleFieldHeight();
+			double dist = Math.sqrt(Math.pow(newX - getX(), 2) + Math.pow(newY - getY(), 2));
+			double angle = Math.atan2(newX - getX(), newY - getY());
+			double turn = angle - getHeadingRadians();
+			while (turn > Math.PI) {
+				turn -= Math.PI * 2;
+			}
+			while (turn < -Math.PI) {
+				turn += Math.PI * 2;
+			}
+			setAhead(dist);
+			setTurnRightRadians(turn);
 		}
 	}
 
@@ -43,14 +58,28 @@ public class RoseBot extends AdvancedRobot
 	 * onScannedRobot: What to do when you see another robot
 	 */
 	public void onScannedRobot(ScannedRobotEvent e) {
-	// Replace the next line with any behavior you would like
-		double angle = getHeading() + e.getBearing();
-		double turn = angle - getGunHeading();
+		double speed = Rules.getBulletSpeed(2.0);
+		double time = e.getDistance() / speed;
+		double x = predictX(e, time);
+		double y = predictY(e, time);
 
-		setTurnGunLeft(turn);
-		double x = xCoord(e);
-		double y = yCoord(e);
-		System.out.println(x + ", " + y);
+		double theta = Math.atan2(x - getX(), y - getY());
+		double dist = Math.sqrt(Math.pow(x - getX(), 2) + Math.pow(y - getY(), 2));
+		double bSpeed = dist / time;
+		double power = (20 - bSpeed) / 3;
+
+		double turn = theta - getGunHeadingRadians();
+
+		while (turn <= -Math.PI) {
+			turn += 2 * Math.PI;
+		}
+		while (turn > Math.PI) {
+			turn -= 2 * Math.PI;
+		}
+		setTurnGunRightRadians(turn);
+		if (Math.abs(turn) < 0.1 && power <= 3.0) {
+			setFire(power);
+		}
 
 	}
 
@@ -58,44 +87,24 @@ public class RoseBot extends AdvancedRobot
 		double angle = getHeadingRadians() + e.getBearingRadians();
 		double dist = e.getDistance() * Math.sin(angle);
 		return dist + getX();
-
 	}
 
 	public double yCoord(ScannedRobotEvent e) {
 		double angle = getHeadingRadians() + e.getBearingRadians();
 		double dist = e.getDistance() * Math.cos(angle);
 		return dist + getY();
-
-	}
-	
-	public double xPredict(ScannedRobotEvent e) {
-		double speed = Rules.getBulletSpeed(2.0);
-		//the speed can be changed from [.1, 3.0]
-		double time = speed * e.getDistance();
-		double x = xCoord(e) + time * e.getVelocity()* Math.sin(e.getHeadingRadians());
-		return x;
 	}
 
-	public double yPredict(ScannedRobotEvent e) {
-		double speed = Rules.getBulletSpeed(2.0);
-		//the speed can be changed from [.1, 3.0]
-		double time = speed * e.getDistance();
-		double y = xCoord(e) + time * e.getVelocity()* Math.cos(e.getHeadingRadians());
-		return y;
+	public double predictX(ScannedRobotEvent e, double time) {
+		double newX = xCoord(e) + time * e.getVelocity() * Math.sin(e.getHeadingRadians());
+		return newX;
 	}
 
-
-
-// Scanned robot gets you name heading, bearing, energy, ... look for more in API
-
-
-	/**
-	 * onHitByBullet: What to do when you're hit by a bullet
-	 */
-	public void onHitByBullet(HitByBulletEvent e) {
-		// Replace the next line with any behavior you would like
-		back(10);
+	public double predictY(ScannedRobotEvent e, double time) {
+		double newY = yCoord(e) + time * e.getVelocity() * Math.cos(e.getHeadingRadians());
+		return newY;
 	}
+
 
 	/**
 	 * onHitWall: What to do when you hit a wall
